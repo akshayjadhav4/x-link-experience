8884136f00a323c39e42840da8b2ace2513283a9
Akshay Jadhav <akshayjadhav461999@gmail.com>
2025-11-01 14:45:40 +0530

    cleanup and native tabs setup

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,59 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import {
+  Icon,
+  NativeTabs as Tabs,
+  VectorIcon,
+} from "expo-router/unstable-native-tabs";
+import React from "react";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Colors } from "@/constants/theme";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { DynamicColorIOS, Platform } from "react-native";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+      // For the selected icon color
+      tintColor={DynamicColorIOS({
+        dark: Colors.dark.tint,
+        light: Colors.light.tint,
+      })}
+    >
+      <Tabs.Trigger name="index">
+        {Platform.select({
+          ios: <Icon sf="house.fill" />,
+          android: (
+            <Icon src={<VectorIcon family={MaterialIcons} name="home" />} />
+          ),
+        })}
+      </Tabs.Trigger>
+      <Tabs.Trigger name="search">
+        {Platform.select({
+          ios: <Icon sf="magnifyingglass" />,
+          android: (
+            <Icon src={<VectorIcon family={MaterialIcons} name="search" />} />
+          ),
+        })}
+      </Tabs.Trigger>
+      <Tabs.Trigger name="notifications">
+        {Platform.select({
+          ios: <Icon sf="bell" />,
+          android: (
+            <Icon
+              src={<VectorIcon family={MaterialIcons} name="notifications" />}
+            />
+          ),
+        })}
+      </Tabs.Trigger>
+      <Tabs.Trigger name="messages">
+        {Platform.select({
+          ios: <Icon sf="envelope" />,
+          android: (
+            <Icon
+              src={<VectorIcon family={MaterialIcons} name="mail-outline" />}
+            />
+          ),
+        })}
+      </Tabs.Trigger>
     </Tabs>
   );
 }
